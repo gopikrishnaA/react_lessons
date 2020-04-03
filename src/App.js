d010657560fd94a6ba2f90ae6388229cb6d15198
Gopi Akkumahanthi <Gopi.Akkumahanthi@ivycomptech.com>
2020-04-03 18:09:01 +0530

    lesson3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NewComponent from './NewComponent'
 
-export default function App() {
-  return (
-    <div className="App">
-      <NewComponent text={'sample'}/> {/* new Component*/}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+export default class App extends Component {
+  // constructor (props) {
+  // super(props)
+  state = {
+    host: ''
+  }
+  // }
+  getData(host) {
+    this.setState({
+      host
+    })
+  }
+  render() {
+    const { host } = this.state
+    return (
+      <div className="App" >
+        <NewComponent />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>host: {host}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
+
--- src/NewComponent.js
+++ src/NewComponent.js
@@ -1,38 +1,65 @@
 import React, { Component } from 'react'
+import Proptypes from 'prop-types'
 
 class NewComponent extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props)
         //props vs state
         this.state = {
-            text: props.text,
-            counter: 0
+            counter: 0,
+            host: ''
         }
         this.update = this.update.bind(this)
     }
-    componentWillMount () {
-        console.log('-----componentWillMount--------')
+    // shouldComponentUpdate (nextProps, nextState) {
+    //     console.log(nextProps, nextState, this.state.counter)
+    //     // if (JSON.stringify(nextProps.text) !== this.props.text) {
+    //     //     return true
+    //     // }
+    //     // retun false
+    // }
+    // componentWillMount() {
+    //     console.log('-----componentWillMount--------')
+    // }
+    static getDerivedStateProps() {
+        
     }
     componentDidMount() {
-        this.setState({
-            text: 'gopi'
-        })
-        console.log('-----componentDidMount--------')
+        //es6 fetch
+        fetch('https://petstore.swagger.io/v2/swagger.json')
+            .then(response => {
+                // pending state
+                return response.json()
+            }).then(data => {
+                // resolve
+                this.setState({
+                    host: data.host
+                })
+                // this.props.getData(data.host)
+            }).catch(err => {
+                // reject 
+                console.error(`err`, err)
+            })
     }
     componentWillUnmount() {
         console.log('-----componentWillUnmount--------')
     }
 
-    update () {
+    update() {
         this.setState({
             counter: this.state.counter + 1
         })
     }
     render() {
         console.log('-----render--------')
-        return (<div>This is {this.state.counter}
-        <button onClick={this.update}>Show</button>
-            </div>)
+        return (<div>
+            This is {this.state.counter} host is {this.state.host}
+            <button onClick={this.update}>Show</button>
+        </div>)
     }
 }
 export default NewComponent
+
+NewComponent.prototypes = {
+    getData: Proptypes.func.isRequired
+  }
