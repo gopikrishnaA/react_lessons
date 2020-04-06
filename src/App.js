895b7b542662c2b43f7bcb53665ecc3f35776d89
Gopi Akkumahanthi <Gopi.Akkumahanthi@ivycomptech.com>
2020-04-06 19:11:00 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NewComponent from './NewComponent'
+import NavMenu from './NavMenu'
+
+import './App.css';
 
 export default class App extends Component {
-  // constructor (props) {
-  // super(props)
-  state = {
-    host: ''
-  }
-  // }
-  getData(host) {
-    this.setState({
-      host
-    })
-  }
+
   render() {
-    const { host } = this.state
     return (
-      <div className="App" >
-        <NewComponent />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>host: {host}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <NavMenu />
+          <Switch>
+            <Route exact path="/">
+              <NewComponent />
+            </Route>
+            <Route path="/about">
+              <div>About</div>
+            </Route>
+            <Route path="/dashboard">
+              <div>dashboard</div>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/NewComponent.js
+++ src/NewComponent.js
@@ -2,64 +2,66 @@ import React, { Component } from 'react'
 import Proptypes from 'prop-types'
 
 class NewComponent extends Component {
-    constructor(props) {
-        super(props)
-        //props vs state
-        this.state = {
-            counter: 0,
-            host: ''
-        }
-        this.update = this.update.bind(this)
+  constructor(props) {
+    super(props)
+    //props vs state
+    this.state = {
+      counter: 0,
+      tags: [],
+      value: ''
     }
-    // shouldComponentUpdate (nextProps, nextState) {
-    //     console.log(nextProps, nextState, this.state.counter)
-    //     // if (JSON.stringify(nextProps.text) !== this.props.text) {
-    //     //     return true
-    //     // }
-    //     // retun false
-    // }
-    // componentWillMount() {
-    //     console.log('-----componentWillMount--------')
-    // }
-    static getDerivedStateProps() {
-        
-    }
-    componentDidMount() {
-        //es6 fetch
-        fetch('https://petstore.swagger.io/v2/swagger.json')
-            .then(response => {
-                // pending state
-                return response.json()
-            }).then(data => {
-                // resolve
-                this.setState({
-                    host: data.host
-                })
-                // this.props.getData(data.host)
-            }).catch(err => {
-                // reject 
-                console.error(`err`, err)
-            })
-    }
-    componentWillUnmount() {
-        console.log('-----componentWillUnmount--------')
-    }
-
-    update() {
+    this.update = this.update.bind(this)
+    this.onChangeListner = this.onChangeListner.bind(this)
+  }
+  componentDidMount() {
+    //es6 fetch
+    fetch('https://petstore.swagger.io/v2/swagger.json')
+      .then(response => {
+        // pending state
+        return response.json()
+      }).then(data => {
+        // resolve
         this.setState({
-            counter: this.state.counter + 1
+          tags: data.tags
         })
-    }
-    render() {
-        console.log('-----render--------')
-        return (<div>
-            This is {this.state.counter} host is {this.state.host}
-            <button onClick={this.update}>Show</button>
-        </div>)
-    }
+        // this.props.getData(data.host)
+      }).catch(err => {
+        // reject 
+        console.error(`err`, err)
+      })
+  }
+  update() {
+    this.setState({
+      tags: [{ name: 'grishma' }, ...this.state.tags]
+    })
+
+    console.log(this.ref.value)
+  }
+
+  onChangeListner (event) {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  render() {
+    const { counter, tags, value } = this.state
+
+    return [<div key='1'>
+      This is {counter}
+      <button onClick={this.update}>Show</button>
+      <input ref={(ref) => this.input = ref}></input>
+      <input onChange={this.onChangeListner}></input>
+      <p>{value}</p>
+    </div>,
+    <ul key='2'>
+      {tags.map((tag) => <li key={tag.name}>{tag.name}</li>)}
+    </ul>
+    ]
+  }
 }
 export default NewComponent
 
 NewComponent.prototypes = {
-    getData: Proptypes.func.isRequired
-  }
+  getData: Proptypes.func.isRequired
+}
